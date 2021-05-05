382141f0eb6421497325695ead0796787b314712
Juan <jlizarraga@unomaha.edu>
2021-05-05 01:58:42 -0500

    Update settings.py

--- chineseSpicyApp/settings.py
+++ chineseSpicyApp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bh65$(^i_4@wmjp_&&3ftc^!b2msaqah=7$99jb!vn11z1k618'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
