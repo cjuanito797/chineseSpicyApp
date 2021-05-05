bdc32610a5165a7428781203a53b8edacf79992d
Juan <jlizarraga@unomaha.edu>
2021-05-05 02:37:55 -0500

    Update models.py

--- orders/models.py
+++ orders/models.py
@@ -23,7 +23,7 @@ class Order(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
-    braintree_id = models.CharField(max_length=150, blank=True, default=None)
+    braintree_id = models.CharField(max_length=150, blank=True)
     DELIVERY_CHOICES = (("DELIVERY", 'Delivery'), ("PICKUP", 'Pickup'))
     delivery_pref = models.CharField(max_length=10, choices=DELIVERY_CHOICES, default="Pickup")
     address = models.ForeignKey(Address,
