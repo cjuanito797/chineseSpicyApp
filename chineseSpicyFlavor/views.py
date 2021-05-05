5fa126cafce4be3fafcc67b9499d968884d79016
Juan <jlizarraga@unomaha.edu>
2021-05-05 11:38:01 -0500

    Update views.py

--- chineseSpicyFlavor/views.py
+++ chineseSpicyFlavor/views.py
@@ -284,6 +284,8 @@ class Sales(PermissionRequiredMixin, generic.ListView):
         totalSales = Sum(data)
     print(totalSales)
 
+    """THis is a comment"""
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(Sales, self).get_context_data(**kwargs)
         context.update({'totalSales' : self.totalSales})
